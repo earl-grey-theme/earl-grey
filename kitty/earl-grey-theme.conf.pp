f0937bbea219713adb174c750fecd858425db079
June Kelly <june@junek.xyz>
2021-07-29 18:10:37 +0100

    refine kitty theme

--- kitty/earl-grey-theme.conf.pp
+++ kitty/earl-grey-theme.conf.pp
@@ -2,38 +2,39 @@
 # Earl Grey
 background            ◊(eg-color "bg")
 foreground            ◊(eg-color "fg")
-cursor                ◊(eg-color "fg")
+cursor                ◊(eg-color "blue8")
+cursor_text_color     ◊(eg-color "bg")
 selection_background  ◊(eg-color "purple2")
 selection_foreground
 
-## Background
-color0 ◊(eg-color "bg")
+## Black
+color0 ◊(eg-color "bg2")
 color8 ◊(eg-color "grey4")
 
 ## Red
 color1 ◊(eg-color "red")
-color9 ◊(eg-color "red1")
+color9 ◊(eg-color "red2")
 
 ## Green
 color2 ◊(eg-color "green")
-color10 ◊(eg-color "green4")
+color10 ◊(eg-color "green2")
 
 ## Yellow
 color3 ◊(eg-color "orange")
-color11 ◊(eg-color "orange4")
+color11 ◊(eg-color "orange2")
 
 ## Blue
 color4 ◊(eg-color "blue")
-color12 ◊(eg-color "blue4")
+color12 ◊(eg-color "blue2")
 
 ## Magenta
 color5 ◊(eg-color "purple")
-color13 ◊(eg-color "purple4")
+color13 ◊(eg-color "purple2")
 
 ## Cyan
 color6 ◊(eg-color "teal")
-color14 ◊(eg-color "teal4")
+color14 ◊(eg-color "teal2")
 
-## Foreground
+## White
 color7 ◊(eg-color "fg")
 color15 ◊(eg-color "grey8")
